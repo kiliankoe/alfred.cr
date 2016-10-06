d0b6cb49bdaed3fbec186a80c04a5dfcb023395f
Kilian Koeltzsch <me@kilian.io>
2016-10-06 20:32:32 +0200

    basic implementation following the alfred docs

--- src/alfred.cr
+++ src/alfred.cr
@@ -1,5 +1,61 @@
 require "./alfred/*"
+require "json"
+require "http/client"
 
 module Alfred
-  # TODO Put your code here
+  def self.input
+    ARGV.size > 0 ? ARGV[0] : ""
+  end
+
+  def self.generate_json(items : Array(Item))
+    json = String.build do |io|
+      io.json_object do |object|
+        object.field "items" do
+          io.json_array do |array|
+            items.each { |item| array << item }
+          end
+        end
+      end
+    end
+  end
+
+  def self.output(items : Array(Item))
+    puts generate_json items
+  end
+
+  def self.log(text : String)
+    puts text if debug
+  end
+
+  def self.error(err : String)
+    item = Item.new
+    item.title = "Error: #{err}"
+    item.subtitle = "Press ⌘L to see the full error and ⌘C to copy it."
+
+    text = Text.new
+    text.copy = err
+    text.largetype = err
+    item.text = text
+
+    output item
+  end
+
+  def self.fetch(url : String)
+    response = HTTP::Client.get url
+    response.body
+  end
+
+  def self.debug : Bool
+    ENV.has_key?("debug")
+  end
+
+  # Helper overload for single items
+  def self.generate_json(item : Item)
+    generate_json [item]
+  end
+
+  # Helper overload for single items
+  def self.output(item : Item)
+    output [item]
+  end
 end
---
+++ src/alfred/item.cr
@@ -0,0 +1,136 @@
+require "json"
+
+module Alfred
+  class Item
+    JSON.mapping(
+      uid: String | Nil,
+      title: String,
+      subtitle: String | Nil,
+      arg: String | ComplexArg | Nil,
+      icon: Icon | Nil,
+      valid: Bool | Nil,
+      autocomplete: String | Nil,
+      type: String | Nil,
+      mods: Hash(String, Mod) | Nil,
+      text: Text | Nil,
+      quicklookurl: String | Nil,
+    )
+
+    def initialize
+      @title = ""
+    end
+
+    def initialize(&block)
+      @title = ""
+      yield self
+    end
+
+    def add_mod(key : String, mod : Mod)
+      mods = @mods
+      if mods.nil?
+        mods = {key => mod}
+      else
+        mods[key] = mod
+      end
+      @mods = mods
+    end
+  end
+
+  class ComplexArg
+    # FIXME: This has to wrapped in {"alfredworkflow": ComplexArg}
+
+    JSON.mapping(
+      arg: String | Nil,
+      variables: Hash(String, String) | Nil,
+    )
+
+    def initialize; end
+
+    def initialize(&block)
+      yield self
+    end
+  end
+
+  class Icon
+    JSON.mapping(
+      type: String | Nil,
+      path: String,
+    )
+
+    def initialize
+      @path = ""
+    end
+
+    def initialize(&block)
+      @path = ""
+      yield self
+    end
+  end
+
+  enum Type
+    Default
+    File
+    FileSkipCheck
+
+    def to_s
+      case self
+      when Type::Default
+        "default"
+      when Type::File
+        "file"
+      when Type::FileSkipCheck
+        "file:skipcheck"
+      end
+    end
+  end
+
+  class Mod
+    JSON.mapping(
+      valid: Bool | Nil,
+      arg: String,
+      subtitle: String | Nil,
+    )
+
+    def initialize
+      @arg = ""
+    end
+
+    def initialize(&block)
+      @arg = ""
+      yield self
+    end
+  end
+
+  enum ModKey
+    Command
+    Option
+    Control
+    CMD
+    ALT
+    CTRL
+
+    def to_s
+      case self
+      when Command, CMD
+        "cmd"
+      when Option, ALT
+        "alt"
+      else
+        "ctrl"
+      end
+    end
+  end
+
+  class Text
+    JSON.mapping(
+      copy: String | Nil,
+      largetype: String | Nil,
+    )
+
+    def initialize; end
+
+    def initialize(&block)
+      yield self
+    end
+  end
+end
